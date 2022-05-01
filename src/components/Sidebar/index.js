c8649de5f16cbfb41a6cabf4279d6abf68d11a96
midhun <midhunn311@gmail.com>
2022-05-01 11:51:17 +0530

    commit 2

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,14 @@
 import './index.scss'
+import {Link} from 'react-router-dom'
 import logoM from '../../assets/images/logo-s.png'
 import logoName from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <link className='logo' to = './'>
+        <Link className='logo' to = './'>
             <img src={logoM} alt="logo"/>
             <img src={logoName} alt="logoname"/>
-        </link>
+        </Link>
     </div>
 )
 
