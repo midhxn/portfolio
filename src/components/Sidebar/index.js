3be31cfe63773a61927570c7bb0b03ad75d49bbd
midhun <midhunn311@gmail.com>
2022-05-02 01:17:05 +0530

    additions

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,27 @@
 import './index.scss'
-import {Link} from 'react-router-dom'
+import {Link,NavLink} from 'react-router-dom'
 import logoM from '../../assets/images/logo-s.png'
 import logoName from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome,faUser,faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to = './'>
+    <div className="nav-bar">
+        <Link className="logo" to = "/">
             <img src={logoM} alt="logo"/>
-            <img src={logoName} alt="logoname"/>
+            <img className="sub-logo" src={logoName} alt="logoname"/>
         </Link>
+        <nav>
+            <NavLink exact="true" activeClassName="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeClassName="active" className="about" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeClassName="active" className="contact" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink> 
+        </nav>
     </div>
 )
 
