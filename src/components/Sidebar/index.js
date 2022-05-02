26ae79d0efefa8fe4b9c4fbf5a0139c4dbed8afd
midhun <midhunn311@gmail.com>
2022-05-02 14:36:35 +0530

    add 02/05

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,9 @@
 import './index.scss'
 import {Link,NavLink} from 'react-router-dom'
 import logoM from '../../assets/images/logo-s.png'
-import logoName from '../../assets/images/logo_sub.png'
+import logoName from '../../assets/images/logosub.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faHome,faUser,faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faHome,faRobot,faEnvelope,faCode} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -16,7 +16,10 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
             </NavLink>
             <NavLink exact="true" activeClassName="active" className="about" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+                <FontAwesomeIcon icon={faRobot} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeClassName="active" className="projects" to="/projects">
+                <FontAwesomeIcon icon={faCode} color="#4d4d4e"/>
             </NavLink>
             <NavLink exact="true" activeClassName="active" className="contact" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
