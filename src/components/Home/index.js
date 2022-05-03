ea987cf5e7df491a47b387953e690ab82d0ddf55
midhun <midhunn311@gmail.com>
2022-05-03 23:58:39 +0530

    commit 03/05

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,11 @@
 const Home = () => {
+    import Logotitle from ./assets/images
 
     return(
         <div className="container home-page">
             <div className="text-zone">
-            <h1>Hi, <br/> I,m </h1>
+                <h1>Hi, <br/> I,m </h1>
+                <img src={} alt="dev"/>
             
             </div>
         
