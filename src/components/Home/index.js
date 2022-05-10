50a24928c64964bd9504587482864a9d477e6201
midhun <midhunn311@gmail.com>
2022-05-11 01:03:49 +0530

    addition of code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,33 @@
 import Logotitle from '../../assets/images/logom.png'
 import {Link} from 'react-router-dom'
+import {useState} from 'react';
 import './index.scss'
+import LetterAnimation from '../LetterAnimation';
 
 const Home = () => {
     
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['i', 'd', 'h', 'u', 'n']
+    const jobArray = ['w', 'e', 'b',' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I,m 
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br/> 
+                    <span className={`${letterClass} _12`}>I</span>
+                    <span className={`${letterClass} _12`}>,m</span>
                 <img src={Logotitle} alt="dev"/>
-                idhun
+                <LetterAnimation letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
                 <br/>
-                web developer </h1>
+                <LetterAnimation letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}/>
+                </h1>
                 <h2>Full Stack Developer | Javascript | Open Source</h2>
                 <Link to="/contact" className='flat-button'>CONTACT Me</Link>
 
